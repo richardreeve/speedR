a533d7426d3a73b77b5790e81c1ed3ccdcb0d9d5
Richard Reeve <git@richardreeve.net>
2016-07-14 12:33:03 +0100

    Show what happens when you compile the code. Speedup, but no visibility in what's happening any longer.

--- profile.r
+++ profile.r
@@ -16,3 +16,22 @@ IntQuirksAprof <- aprof("interpreter.r","interpreter.out")
 IntQuirksAprof
 plot(IntQuirksAprof)
 summary(IntQuirksAprof)
+
+library(compiler)
+
+compiled <- cmpfun(InterpreterQuirks)
+Rprof(file="compiled.out",
+      interval=0.02,
+      line.profiling=TRUE)
+
+compiled(N=1e7) # run 10 million times
+
+Rprof(append=FALSE) # stop profiling
+
+summaryRprof("compiled.out")
+
+compiledSpeed <- aprof("interpreter.r","compiled.out")
+
+compiledSpeed
+plot(compiledSpeed)
+summary(compiledSpeed)
